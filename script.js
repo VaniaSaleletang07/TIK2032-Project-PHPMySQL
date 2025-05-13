3ec90d85bdf9249734920ba2e00a4f97d114bf47
VaniaSaleletang07 <vaniasaleletang026@student.unsrat.ac.id>
2025-05-13 12:52:13 +0700

    membuat animasi notifikasi

--- script.js
+++ script.js
@@ -59,3 +59,17 @@ skillBars.forEach(bar => {
   lightbox.addEventListener('click', () => {
     lightbox.classList.remove('show');
   });
+
+
+function showNotification(message) {
+    const notification = document.getElementById('notification');
+    notification.textContent = message;
+    notification.classList.add('show');
+    notification.classList.remove('hidden');
+
+    // Hilangkan notifikasi setelah 3 detik
+    setTimeout(() => {
+        notification.classList.add('hidden');
+        notification.classList.remove('show');
+    }, 3000);
+}
