d7c85e4e1068088ff73bb27cf9f67069002bb121
VaniaSaleletang07 <vaniasaleletang026@student.unsrat.ac.id>
2025-05-13 19:10:43 +0700

    menghubungkan dengan file script.js

--- artikel.php
+++ artikel.php
@@ -28,6 +28,7 @@ $article = mysqli_fetch_assoc($result);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $article['title']; ?></title>
     <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script>
 </head>
 <body>
     <main class="artikel-container">
